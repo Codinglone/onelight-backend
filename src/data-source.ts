b04a5d688790bf7b3db58a900cf70cf72004f1d9
Codinglone <codinglone@gmail.com>
2023-09-04 11:12:01 +0200

    Added dotenv config

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,17 @@
+require('dotenv').config()
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 
+const PORT = parseInt(process.env.DB_PORT) || 5000
+
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    host: process.env.DB_HOST,
+    port: PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     entities: [User],
