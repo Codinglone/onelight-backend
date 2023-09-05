4efb9b060dfe0379e7cbeb2f205917fdfa3da5e8
Codinglone <codinglone@gmail.com>
2023-09-05 12:25:29 +0200

    Added Gallery Entity

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,20 +1,21 @@
-require('dotenv').config()
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+require("dotenv").config();
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+import { Gallery } from "./entity/Gallery";
 
-const PORT = parseInt(process.env.DB_PORT) || 5000
+const PORT = parseInt(process.env.DB_PORT) || 5000;
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: process.env.DB_HOST,
-    port: PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: [User, Gallery],
+  migrations: [],
+  subscribers: [],
+});
